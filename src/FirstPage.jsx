c29e77e503cf54229eaf6333d168661e64c60ab5
YoussefAhmed108 <youssef.ahmed108@gmail.com>
2024-04-23 16:41:37 +0200

    ga_events

--- src/FirstPage.jsx
+++ src/FirstPage.jsx
@@ -1,18 +1,42 @@
-import React from 'react'; // Import the external CSS file
+// FirstPage.js
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
-const FirstPage = ({ block }) => {
-    const navigate = useNavigate()
-    return (
-        <div>
-            <h2>Welcome to the website</h2>
-            <div>
-                <button onClick={() => navigate('/second')}>Go to second(2) page</button>
-                <button>Add to cart</button>
-                <button>Checkout</button>
-            </div>
-        </div>
-    );
+const FirstPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    ReactGA.event({
+      category: 'Page Interaction',
+      action: 'View First Page'
+    });
+  }, []);
+
+  const handleAddToCart = () => {
+    ReactGA.event({
+      category: 'Conversion',
+      action: 'Add to Cart'
+    });
+  };
+
+  const handleCheckout = () => {
+    ReactGA.event({
+      category: 'Conversion',
+      action: 'Checkout'
+    });
+  };
+
+  return (
+    <div>
+      <h2>Welcome to the website</h2>
+      <div>
+        <button onClick={() => navigate('/second')}>Go to second page</button>
+        <button onClick={handleAddToCart}>Add to cart</button>
+        <button onClick={handleCheckout}>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default FirstPage;
--- src/SecondPage.jsx
+++ src/SecondPage.jsx
@@ -1,15 +1,26 @@
-import { useNavigate } from "react-router-dom";
+// SecondPage.js
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
-const SecondPage = ({ block }) => {
-    const navigate = useNavigate()
-    return (
-        <div>
-            <h2>Welcome to the second page</h2>
-            <div>
-                <button onClick={() => navigate('/')}>Go to first page</button>
-            </div>
-        </div>
-    );
+const SecondPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    ReactGA.event({
+      category: 'Page Interaction',
+      action: 'View Second Page'
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Welcome to the second page</h2>
+      <div>
+        <button onClick={() => navigate('/')}>Go to first page</button>
+      </div>
+    </div>
+  );
 };
 
 export default SecondPage;
